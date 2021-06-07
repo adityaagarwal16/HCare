2cdbd071e6f68c7a10b4f8ddf63dbd46983425c0
aditya <adityaagarwal990@gmail.com>
2021-06-08 00:38:12 +0530

    2.0
    Signed-off-by: aditya <adityaagarwal990@gmail.com>

--- app/src/androidTest/java/com/hcare/homeopathy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hcare/homeopathy/ExampleInstrumentedTest.java
@@ -7,8 +7,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -19,10 +17,9 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.hcare.hcare", appContext.getPackageName());
     }
 }
