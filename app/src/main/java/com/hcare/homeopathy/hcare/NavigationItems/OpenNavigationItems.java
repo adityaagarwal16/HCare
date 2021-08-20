53895703415140b59e0610bcb6831c52f53eff34
l3limp <kakarvardaan@gmail.com>
2021-08-21 00:40:41 +0530

    added dynamic link

--- app/src/main/java/com/hcare/homeopathy/hcare/NavigationItems/OpenNavigationItems.java
+++ app/src/main/java/com/hcare/homeopathy/hcare/NavigationItems/OpenNavigationItems.java
@@ -3,7 +3,19 @@ package com.hcare.homeopathy.hcare.NavigationItems;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
 import com.hcare.homeopathy.hcare.Consultations.AllChatsActivity;
 import com.hcare.homeopathy.hcare.Main.CoronaVirusActivity;
 import com.hcare.homeopathy.hcare.Main.Doctors.DoctorsActivity;
@@ -14,6 +26,9 @@ import com.hcare.homeopathy.hcare.R;
 public class OpenNavigationItems {
 
     Context context;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String uid = user.getUid();
+    Uri mInvitationUrl;
     int id;
 
     public OpenNavigationItems(Context context, int id) {
@@ -48,16 +63,7 @@ public class OpenNavigationItems {
                 break;
             case R.id.invite:
                 intent = null;
-                try {
-                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                    shareIntent.setType("text/plain");
-                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name));
-                    String shareMessage= "Check out HCare!! India's first only Homeopathic " +
-                            "online consultation and delivery app \n\n"
-                            + "https://play.google.com/store/apps/details?id=com.hcare.homeopathy.hcare";
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
-                    context.startActivity(Intent.createChooser(shareIntent, "choose one"));
-                } catch(Exception e) { e.printStackTrace(); }
+                refer();
                 break;
 
             case R.id.rateUs:
@@ -74,6 +80,49 @@ public class OpenNavigationItems {
         }
         if(intent != null)
             context.startActivity(intent);
+    }
+
+    public void refer() {
+
+//        String link = "https://hcare.com/?invitedby=" + uid;
+//        FirebaseDynamicLinks.getInstance().createDynamicLink()
+//                .setLink(Uri.parse(link))
+//                .setDomainUriPrefix("https://hcare.page.link") // after registering on firebase, add link here
+//                .setAndroidParameters(
+//                        new DynamicLink.AndroidParameters.Builder("com.hcare.android")
+//                                .build())
+//                .setIosParameters(
+//                        new DynamicLink.IosParameters.Builder("com.hcare.homeopathy.hcare.ios")
+//                                .build())
+//                .buildShortDynamicLink()
+//                .addOnSuccessListener(new OnSuccessListener<ShortDynamicLink>() {
+//                    @Override
+//                    public void onSuccess(@NonNull ShortDynamicLink shortDynamicLink) {
+//                        mInvitationUrl = shortDynamicLink.getShortLink();
+//                        // ...
+//                    }
+//                });
+        DynamicLink dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(Uri.parse("https://hcare.com/?invitedby=" + uid))
+                .setDomainUriPrefix("https://hcare.page.link")
+                .setAndroidParameters(new DynamicLink.AndroidParameters.Builder().build())
+                .setIosParameters(new DynamicLink.IosParameters.Builder("com.example.ios").build())
+                .buildDynamicLink();
+
+        Uri dynamicLinkUri = dynamicLink.getUri();
+
+
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name));
+            String shareMessage= "Check out HCare!! India's first only Homeopathic " +
+                    "online consultation and delivery app \n\n"
+                      + dynamicLinkUri.toString();
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            context.startActivity(Intent.createChooser(shareIntent, "How do you want to share"));
+
+        } catch(Exception e) { e.printStackTrace(); }
     }
 
 }
--- app/src/main/java/com/hcare/homeopathy/hcare/Start/LoginActivity.java
+++ app/src/main/java/com/hcare/homeopathy/hcare/Start/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.hcare.homeopathy.hcare.Start;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.hcare.homeopathy.hcare.BaseActivity;
 import com.hcare.homeopathy.hcare.R;
 import com.hcare.homeopathy.hcare.Start.Home.LoginHomeFragment;
@@ -24,6 +33,8 @@ public class LoginActivity extends BaseActivity {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        receiveReferral();
 
         try {
             getSupportFragmentManager()
@@ -79,5 +90,24 @@ public class LoginActivity extends BaseActivity {
         }
         return customView;
     };*/
+
+    public void receiveReferral() {
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, pendingDynamicLinkData -> {
+                    Uri deepLink = null;
+                    if (pendingDynamicLinkData != null) {
+                        deepLink = pendingDynamicLinkData.getLink();
+                    }
+
+                    assert deepLink != null;
+                    String referLink = deepLink.toString();
+
+                    String custID = referLink.substring(referLink.lastIndexOf("%")+1);
+//                    Toast.makeText(this, custID, Toast.LENGTH_SHORT).show();
+
+                })
+                .addOnFailureListener(this, e -> Log.w("main", "getDynamicLink:onFailure", e));
+    }
 
 }
